8f9e807db62f65918cf4d2e5255ccc5bffe2af6d
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-06-16 00:53:48 -0500

    Create unit tests for softmax
    Tested basic softmax correctness, whether softmax
    generated an array of probabilities of the correct
    range and shape, whether equal inputs to softmax
    yielded a uniform distribution, whether or not
    the softmax function broke for large values, and
    whether or not softmax handles sharp peaks in
    values (ie [0, 1000, 0]) correctly.

--- tests/bandits/policies/test_softmax.py
+++ tests/bandits/policies/test_softmax.py
@@ -4,4 +4,27 @@ from rlmathtoolkit.bandits.policies import softmax, softmax_sample
 def test_softmax_normalization():
     h = [1.0, 2.0, 3.0]
     pi = softmax(h)
-    assert np.sum(pi) == 1
+    assert np.isclose(np.sum(pi), 1.0)
+
+def test_softmax_shape_and_range():
+    h = [1.0, 2.0, 3.0, 5.0, 9.0]
+    pi = softmax(h)
+    assert len(pi) == len(h)
+    assert np.all(pi >= 0) and np.all(pi <= 1)
+
+def test_softmax_uniform_output():
+    h = [1.0, 1.0, 1.0, 1.0]
+    pi = softmax(h)
+    expected = [.25, .25, .25, .25]
+    assert np.allclose(pi, expected)
+
+def test_softmax_large_values():
+    h = [10000, 23300, 2010]
+    pi = softmax(h)
+    assert np.isclose(np.sum(pi), 1.0)
+
+def test_softmax_sharp_peak():
+    h = [1, 10000, 2]
+    pi = softmax(h)
+    assert np.argmax(pi) == 1
+    assert pi[1] > 0.99
